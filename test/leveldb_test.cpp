254dd7b6266ee0e082b976387440bb8617d21543
tanshun <tanshunwork@gmail.com>
2022-07-06 15:38:51 +0800

    test: add level_db write batch test

--- test/leveldb_test.cpp
+++ test/leveldb_test.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 #include "leveldb/db.h"
+#include "leveldb/write_batch.h"
 
 using namespace std;
 using namespace leveldb;
@@ -20,5 +21,30 @@ TEST(LeveldbTest, Basic) {
     assert(status.IsNotFound());
     cout << res << endl;
 
+    delete db;
+}
+
+TEST(LeveldbTest, WriteBatch) {
+    leveldb::DB *db;
+    leveldb::Options options;
+    options.create_if_missing = true;
+    leveldb::Status status = leveldb::DB::Open(options, "testdb", &db);
+    assert(status.ok());
+
+    leveldb::WriteBatch wb;
+
+    wb.Put("k1", "v1");
+    wb.Put("k2", "v2");
+
+    db->Write(leveldb::WriteOptions(), &wb);
+    std::string v1;
+    status = db->Get(leveldb::ReadOptions(), "k1", &v1);
+    assert(status.ok());
+    assert(v1 == "v1");
+
+    std::string v2;
+    status = db->Get(leveldb::ReadOptions(), "k2", &v2);
+    assert(status.ok());
+    assert(v2 == "v2");
     delete db;
 }
