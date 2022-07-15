7e63a589cadf5019db59d4947d60be29cd52796d
Qi Wang <wangqiim@163.com>
2022-07-15 03:56:35 -0700

    mmapsize = (1 << 28) + (1 << 22) is  272629760

--- src/include/def.h
+++ src/include/def.h
@@ -8,13 +8,13 @@ constexpr const int RecordSize = 8 + 128 + 128 + 8;
 constexpr const int kBlockSize = 32768;
 const char WALFileNamePrefix[] = "WAL";
 const int PoolSize = 1 << 29; // 512MB can't exceed 1GB
-const int MmapSize = (1 << 28) + (1 << 27); // 512MB
+const int MmapSize = (1 << 28) + (1 << 22); // 272629760 data: 272000000
 
 // ------ engine.h -------
 const int WritePerClient = 1000000; 
 const int ClientNum = 50;
-const int SSDNum = 50;  // 在lockfree情况下，必须ClientNum = SSDNum + AEPNum
-const int AEPNum = 0;  // 在lockfree情况下，必须ClientNum = SSDNum + AEPNum
+const int SSDNum = 30;  // 在lockfree情况下，必须ClientNum = SSDNum + AEPNum
+const int AEPNum = 20;  // 在lockfree情况下，必须ClientNum = SSDNum + AEPNum
 
 const int WaitChangeFinishSecond = 3;
 const int FenceSecond = 10;
