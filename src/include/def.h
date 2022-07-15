50fb5d3889f7aec54965bbf7d1ab7a6f8e890aaf
Qi Wang <wangqiim@163.com>
2022-07-15 04:09:15 -0700

    adjust params

--- src/include/def.h
+++ src/include/def.h
@@ -13,8 +13,8 @@ const int MmapSize = (1 << 28) + (1 << 22); // 272629760 data: 272000000
 // ------ engine.h -------
 const int WritePerClient = 1000000; 
 const int ClientNum = 50;
-const int SSDNum = 30;  // 在lockfree情况下，必须ClientNum = SSDNum + AEPNum
-const int AEPNum = 20;  // 在lockfree情况下，必须ClientNum = SSDNum + AEPNum
+const int SSDNum = 40;  // 在lockfree情况下，必须ClientNum = SSDNum + AEPNum
+const int AEPNum = 10;  // 在lockfree情况下，必须ClientNum = SSDNum + AEPNum
 
 const int WaitChangeFinishSecond = 3;
 const int FenceSecond = 10;
