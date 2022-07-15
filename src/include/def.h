3a0e80775fcea18baab2d859474203231fe07a93
Qi Wang <wangqiim@163.com>
2022-07-15 09:14:36 -0700

    adjust mmapsize

--- src/include/def.h
+++ src/include/def.h
@@ -8,7 +8,7 @@ constexpr const int RecordSize = 8 + 128 + 128 + 8;
 constexpr const int kBlockSize = 32768;
 const char WALFileNamePrefix[] = "WAL";
 const int PoolSize = 1 << 29; // 512MB can't exceed 1GB
-const int MmapSize = (1 << 28) + (1 << 22); // 272629760 data: 272000000
+const int MmapSize = (1 << 28) + (1 << 21) + (1 << 20) + (1 << 19); // 272629760 data: 272000000
 
 // ------ engine.h -------
 const int WritePerClient = 1000000; 
