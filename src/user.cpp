42c128a3d8723e2de0309262cd10963b82fbe6f0
tanshun <tanshunwork@gmail.com>
2022-07-17 17:12:48 +0800

    freat: use robin_hood hash

--- src/user.cpp
+++ src/user.cpp
@@ -1,16 +1,11 @@
 // Copyright [2018] Alibaba Cloud All rights reserved
 
 #include "user.h"
+#include "robin_hood.h"
 
 static const int kA = 54059;  // a prime
 static const int kB = 76963;  // another prime
 static const int kFinish = 37;  // also prime
 uint32_t StrHash(const char* s, int size) {
-  uint32_t h = kFinish;
-  while (size > 0) {
-    h = (h * kA) ^ (s[0] * kB);
-    s++;
-    size--;
-  }
-  return h;
+  return robin_hood::hash_bytes(s, size);
 }
